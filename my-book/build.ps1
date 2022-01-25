1984d2761c4522524c467d83f18ce27537961329
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2022-01-25 23:14:51 +0300

    Add sample footer

--- my-book/build.ps1
+++ my-book/build.ps1
@@ -11,4 +11,4 @@ else{
 
 Get-Content $ChaptersFolder/*.md | Set-Content $OutputFolder/merged.md
 pandoc -f gfm -t html5 --metadata pagetitle="My Book" --css style.css $OutputFolder/merged.md -o $OutputFolder/result.html --self-contained
-pandoc -f gfm -t html5 --metadata pagetitle="My Book" --css style.css $OutputFolder/merged.md -o $OutputFolder/result.pdf -V margin-top=0 -V margin-left=0 -V margin-right=0 -V margin-bottom=0
+pandoc -f gfm -t html5 --metadata pagetitle="My Book" --css style.css $OutputFolder/merged.md -o $OutputFolder/result.pdf -V margin-left=0 -V margin-right=0 -V margin-top=0 -V margin-bottom=10 -V footer-html=layout/footer.html
